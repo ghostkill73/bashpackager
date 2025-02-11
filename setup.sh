298b874016b686f227aba183412d10fc7df0fa0b
ghostkill73 <142703278+ghostkill73@users.noreply.github.com>
2025-02-11 02:22:12 -0300

    backup

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+project_name="bashpackager_test"
+version="1.0.0"
+description="bashpackager testing"
---
+++ src/main.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+import builtins
+
+lower_case "STRING"
